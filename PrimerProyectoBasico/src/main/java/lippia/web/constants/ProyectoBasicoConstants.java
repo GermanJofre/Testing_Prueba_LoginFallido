32ae314f3a51d4451b6cd9933512fc97ab88e2f1
GermanJofre <germanjofre.27@gmail.com>
2023-01-30 21:48:11 -0300

    Busqueda de juegos

---
+++ PrimerProyectoBasico/src/main/java/lippia/web/constants/ProyectoBasicoConstants.java
@@ -0,0 +1,7 @@
+package lippia.web.constants;
+
+public class ProyectoBasicoConstants {
+    public static final String BUSQUEDA = "xpath:/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input";
+    public static final String RESULTADO = "name:q";
+
+}
--- PrimerProyectoBasico/src/test/resources/web/features/PrimerProyectoBasico.java
+++
@@ -1,5 +0,0 @@
-public class PrimerProyectoBasico {
-    @io.cucumber.java.en.Given("^Como usuario estoy en la main page de google$")
-    public void comoUsuarioEstoyEnLaMainPageDeGoogle() {
-    }
-}
